a8ca10c1011f0417d863ac1d1ffd78428f53947d
unknown <cinthiacaroline205@gmail.com>
2023-06-13 18:29:13 -0300

    Atualização incluindo CSS

--- Blog Pessoal/blogpessoal/src/pages/home.tsx
+++ Blog Pessoal/blogpessoal/src/pages/home.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import './home.css'
 
 function Home() {
     return(
-    <h1>Hello world</h1>
+    <div>
+    <h1 className="titulo">Hello world</h1>
+    <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" width="500" height="500"/>
+    </div>
     )
 }
 
