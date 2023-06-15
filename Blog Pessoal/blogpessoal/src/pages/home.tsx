58fd925b8b9899a082b9a0c2ef2378506d5c66ee
unknown <cinthiacaroline205@gmail.com>
2023-06-14 23:50:02 -0300

    Adicionando footer e navbar

--- Blog Pessoal/blogpessoal/src/pages/home.tsx
+++ Blog Pessoal/blogpessoal/src/pages/home.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import './home.css'
+import React from 'react';
+import {Typography, Grid, Button} from '@material-ui/core';
+import {Box} from '@mui/material';
+import './home.css';
 
 function Home() {
-    return(
-    <div>
-    <h1 className="titulo">Hello world</h1>
-    <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" width="500" height="500"/>
-    </div>
-    )
+    return (
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "pink" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "black", fontWeight: "bold" }}>Seja bem-vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "black", fontWeight: "bold" }}>Aqui é o lugar onde você pode se expressar!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "black", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
+        </>
+    );
 }
 
 export default Home;
